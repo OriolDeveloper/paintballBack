b34475ca128a01caa946f3547f707edbb2abcd83
OriolDeveloper <oriolpereztrascasas@gmail.com>
2025-03-20 13:31:52 +0100

    Configuración SpringBoot
    Configuración inicial springboot

---
+++ src/main/java/com/paintballProject/paintballBack/PaintballBackApplication.java
@@ -0,0 +1,13 @@
+package com.paintballProject.paintballBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintballBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintballBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paintballProject/paintballBack/PaintballBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paintballProject.paintballBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintballBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
